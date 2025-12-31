d74b3184b68d5151abb1fbd5347d002a0d6966a1
Егор Гроцкий <e.grockiy@domyland.com>
2025-12-31 18:48:56 +0300

    Fixed app router (after rebase)

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/extensions/theme_extensions.dart';
 import '../shared/design/app_theme.dart';
 import 'navigation/router.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
     return MaterialApp.router(
       routerConfig: router,
       theme: AppTheme.light(),
@@ -22,8 +25,10 @@ class App extends StatelessWidget {
 
         final style = SystemUiOverlayStyle(
           statusBarColor: Colors.transparent,
-          statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
-          statusBarBrightness: isDark ? Brightness.dark : Brightness.light,
+          statusBarIconBrightness:
+          isDark ? Brightness.light : Brightness.dark,
+          statusBarBrightness:
+          isDark ? Brightness.dark : Brightness.light,
 
           systemNavigationBarColor: navBarColor,
           systemNavigationBarIconBrightness:
