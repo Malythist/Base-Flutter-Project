aa561aeab109cd74270ccd0bfb160f8232884a68
Егор Гроцкий <e.grockiy@domyland.com>
2025-12-31 18:43:25 +0300

    Updated colors and texts parsing for themes

--- lib/core/extensions/theme_extensions.dart
+++ lib/core/extensions/theme_extensions.dart
@@ -1,6 +1,54 @@
 import 'package:flutter/material.dart';
+import '../../shared/design/app_colors.dart';
 
-extension ThemeX on BuildContext {
-  ColorScheme get colors => Theme.of(this).colorScheme;
-  TextTheme get text => Theme.of(this).textTheme;
+extension ThemeExtensions on BuildContext {
+  AppThemeColors get colors => AppThemeColors._(this);
+  AppThemeText get text => AppThemeText._(this);
+}
+
+class AppThemeColors {
+  AppThemeColors._(this._context);
+
+  final BuildContext _context;
+
+  ThemeData get _theme => Theme.of(_context);
+  ColorScheme get _scheme => _theme.colorScheme;
+
+  bool get _isDark => _scheme.brightness == Brightness.dark;
+
+  Color get primary => _scheme.primary;
+  Color get onPrimary => _scheme.onPrimary;
+  Color get background => _scheme.background;
+  Color get surface => _scheme.surface;
+  Color get onSurface => _scheme.onSurface;
+
+  Color get surfaceVariant => _scheme.surfaceContainerHighest;
+
+  Color get gray50 => _isDark ? AppColors.grayDark50 : AppColors.grayLight50;
+  Color get gray100 => _isDark ? AppColors.grayDark100 : AppColors.grayLight100;
+  Color get gray200 => _isDark ? AppColors.grayDark200 : AppColors.grayLight200;
+  Color get gray300 => _isDark ? AppColors.grayDark300 : AppColors.grayLight300;
+  Color get gray400 => _isDark ? AppColors.grayDark400 : AppColors.grayLight400;
+  Color get gray500 => _isDark ? AppColors.grayDark500 : AppColors.grayLight500;
+  Color get gray600 => _isDark ? AppColors.grayDark600 : AppColors.grayLight600;
+
+  Color get gray20 => gray200;
+  Color get gray40 => gray500;
+}
+
+class AppThemeText {
+  AppThemeText._(this._context);
+
+  final BuildContext _context;
+
+  TextTheme get _text => Theme.of(_context).textTheme;
+
+  TextStyle? get headlineSmall => _text.headlineSmall;
+
+  TextStyle? get headlineMedium => _text.headlineMedium;
+  TextStyle? get bodyLarge => _text.bodyLarge;
+
+  TextStyle? get titleMedium => _text.titleMedium;
+  TextStyle? get labelMedium => _text.labelMedium;
+  TextStyle? get bodySmall => _text.bodySmall;
 }
